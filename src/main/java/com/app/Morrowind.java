16bcc52d64294bc4dff5ce5b6f8f6ece97ebdb91
Erin Bergman <erin.e.bergman@gmail.com>
2016-06-23 12:59:20 -0400

    06/23/16

--- src/main/java/com/app/Morrowind.java
+++ src/main/java/com/app/Morrowind.java
@@ -1,35 +1,53 @@
 package com.app;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Scanner;
-
-import com.story.Story001;
 
 public class Morrowind {
 
 	public static Scanner SB = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		
+		try {
+			Desktop userDesktop = java.awt.Desktop.getDesktop();
+			URI url = new URI("http://localhost:8080/morrowind/index.html");
+			userDesktop.browse(url);
 
-		String newGame;
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
 
-		System.out.println("Welcome to Choose Your Morrowind Adventure!\n");
-
-		do {
-
-			System.out.println("Would you like to start a new game?\n" + "Press (Y/N)");
-			newGame = SB.nextLine();
-
-			newGame = Validator.newGame(newGame);
-
-			if ((newGame).equalsIgnoreCase("n")) {
-				System.exit(0);
-			}
-
-		} while (!(newGame).equalsIgnoreCase("y"));
-
-		System.out.println("\nMORROWIND\n\n");
-
-		Story001.instructions();
+		} catch (URISyntaxException ex) {
+			ex.printStackTrace();
+			System.out.println(ex.getLocalizedMessage());
+		}
+		
+//		
+//
+//		String newGame;
+//
+//		System.out.println("Welcome to Choose Your Morrowind Adventure!\n");
+//
+//		do {
+//
+//			System.out.println("Would you like to start a new game?\n" + "Press (Y/N)");
+//			newGame = SB.nextLine();
+//
+//			newGame = Validator.newGame(newGame);
+//
+//			if ((newGame).equalsIgnoreCase("n")) {
+//				System.exit(0);
+//			}
+//
+//		} while (!(newGame).equalsIgnoreCase("y"));
+//
+//		System.out.println("\nMORROWIND\n\n");
+//
+//		Story001.instructions();
 		SB.close();
 
 	}
